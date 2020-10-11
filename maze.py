d21d3fb86b7a88862330a0b3003b51df4e3e3a96
Kim Paolo <kim@Kims-MacBook-Pro-2.local>
2020-10-11 15:58:52 +0800

    add explored path

--- maze.py
+++ maze.py
@@ -12,9 +12,11 @@ class Maze:
         self.cols, self.rows = 0, 0
         self.start, self.finish = None, None
 
+        self.build()
+
     def build(self):
         row = 0
-        with open('mazes/maze2.txt', 'r') as file:
+        with open('/Users/kim/Sites/python/maze/mazes/maze2.txt', 'r') as file:
             column = []
             for char in file.read():
                 if char == 'A':
@@ -36,10 +38,13 @@ class Maze:
 
         return self.maze
 
-    def visualize(self, label = False):
+    def visualize(self, label=False, explored=False):
         bg, bs = 2, 75
         dimension = ((len(self.maze[0])) * (bs), (len(self.maze)) * (bs))
-        colors = {'#': (30, 34, 30), 'A': (174, 23, 0), 'B': (46, 193, 22), '*': (218, 252, 102), ' ': (255, 255, 255)}
+        colors = {'#': (30, 34, 30), 'A': (174, 23, 0), 'B': (46, 193, 22), '*': (218, 252, 102), ' ': (255, 255, 255), 'X': (255, 255, 255)}
+
+        if explored:
+            colors['X'] = (153, 118, 150)
 
         img = Image.new('RGB', dimension)
         drw = ImageDraw.Draw(img)
@@ -50,7 +55,7 @@ class Maze:
                 drw.rectangle(pos, colors[cv], outline=(0, 0, 0), width=bg)
 
                 if label:
-                    font = ImageFont.truetype('assets/Lato-Bold.ttf', 14)
+                    font = ImageFont.truetype('/Users/kim/Sites/python/maze/assets/Lato-Bold.ttf', 14)
                     drw.text(((ck * bs) + 10, (rk * bs) + 10), f'{rk}, {ck}', font=font, fill=(189, 195, 199))
         img.show()
 
@@ -73,20 +78,33 @@ class Maze:
 
         return neighbors
 
-    def solve(self):
+    def solve(self, explored=False):
+        checkpoint = {}
         self.frontier = self.neighbors(self.start)
 
         while len(self.frontier) != 0:
             row, col = self.frontier[-1]
             if self.maze[row][col] == 'B':
                 break
+            self.maze[row][col] = 'X'
+            self.explored.append(self.frontier.pop())
+
+            neighbors = self.neighbors((row, col))
+            if len(neighbors) != 1:
+                for n in neighbors:
+                    checkpoint[n] = self.explored.index((row, col)) + 1
+            self.frontier += filter(lambda n: n not in self.frontier, neighbors)
+
+            if (row, col) in checkpoint:
+                x = checkpoint[(row, col)]
+                self.explored = self.explored[0:x]
+                self.explored.append((row, col))
+
+        for (row, col) in self.explored:
             self.maze[row][col] = '*'
-            self.frontier.pop()
-            self.frontier += filter(lambda n: n not in self.frontier, self.neighbors((row, col)))
+
+        self.visualize(explored=explored)
 
 if __name__ == '__main__':
     maze = Maze()
-
-    maze.build()
     maze.solve()
-    maze.visualize(label=False)
